41fde1f233581d98445d86cbe59b05ca2dec0e31
kcaparas1630 <kcaparas1630@gmail.com>
2024-05-08 17:09:30 -0700

    Added vite-plugin-pwa, and configured vite.config to specfiy registration type for the service worker

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    VitePWA({registerType:'autoUpdate'}), //configures vite plugin for PWA, specifies the registration type for the service worker
+    react()
+  ],
 })
