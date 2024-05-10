f6b45443908739f3e24968e83cad45c01ac8120a
GDamaso <80939533+GDamaso@users.noreply.github.com>
2024-05-10 12:06:16 -0700

    feat: Nmp 22 init express api (#45)
    * backend project initialized; Express/Typescript modules installed
    
    * backend root is /api and /api/health returns code 200 with 'NMP API is healthy and ready!'
    
    * Reorganized folder structure: controllers|routers|models|src
    
    * Included middleware and nodemon for hot reloading; Inclusions: cookie-parser; compression; morgan; cors; note: Helmet is advised for improved security but not yet included
    
    * Added api-router which now routes to health-api-controllercentering routing logic in one place
    
    * Updated tsconfig to watch all .ts files in backend folder
    
    * Reorganized folder structure to follow 'openshift-quickstar' pipeline; package.json updated
    
    * Reversed file name convention from dot to hifen; Added .env implemented with DotEnv; Added a simple .env.template; Halfway of commenting existing code;
    
    * Added README.md with basic environment variables table for reference; Added more comments;

---
+++ backend/src/express.ts
@@ -0,0 +1,24 @@
+// Middleware is activated here
+// Any processing of HTTP Requests is done here 
+// before routing them to their target endpoints
+import express, {Request, Response } from 'express';
+import cookieParser from 'cookie-parser';
+import compression from 'compression';
+import morgan from 'morgan';
+import cors from 'cors';
+import apiRouter from './routes/api-router';
+
+const app = express();
+
+// Express middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(compression());    // Compressor of body data, improving transfer speeds
+app.use(morgan('dev'));    // Logger Requests and Responses in the console
+app.use(cors());           // Activate CORS, allowing access 
+
+// Routes
+app.use('/api', apiRouter);
+
+export default app;
