55a32f693869b4059ee6e663b37e05654842c35d
kcaparas1630 <kcaparas1630@gmail.com>
2024-05-13 10:12:52 -0700

    fixed linter problems with vite.config.ts, installed pwa and other react plugins as dependencies and not devdependencies

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,10 @@
-import { defineConfig } from 'vite'
-import { VitePWA } from 'vite-plugin-pwa'
-import react from '@vitejs/plugin-react-swc'
-
+import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
+import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    VitePWA({registerType:'autoUpdate'}), //configures vite plugin for PWA, specifies the registration type for the service worker
-    react()
+    VitePWA({ registerType: 'autoUpdate' }), // configures vite plugin for PWA, specifies the registration type for the service worker
+    react(),
   ],
-})
+});
