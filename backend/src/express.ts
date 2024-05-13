08899636b47299e4361551a960f30e47e7432186
GDamaso <guildamaso@gmail.com>
2024-05-13 09:57:42 -0700

    Swagger middleware is now in express.ts instead of api-router.ts

--- backend/src/express.ts
+++ backend/src/express.ts
@@ -1,12 +1,14 @@
 // Middleware is activated here
 // Any processing of HTTP Requests is done here 
 // before routing them to their target endpoints
-import express, {Request, Response } from 'express';
+import express from 'express';
 import cookieParser from 'cookie-parser';
 import compression from 'compression';
 import morgan from 'morgan';
 import cors from 'cors';
-import path from 'path';
+import swaggerUi from 'swagger-ui-express';
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerConfig from './config/swaggerConfig';
 import apiRouter from './routes/api-router';
 
 const app = express();
@@ -18,6 +20,13 @@ app.use(cookieParser());
 app.use(compression());    // Compressor of body data, improving transfer speeds
 app.use(morgan('dev'));    // Logger Requests and Responses in the console
 app.use(cors());           // Activate CORS, allowing access 
+app.use(
+  '/api/api-docs',
+  swaggerUi.serve,
+  swaggerUi.setup(
+    swaggerJSDoc(swaggerConfig),
+  ),
+);
 
 // Routes
 app.use('/api', apiRouter);
