20b4c83a08e2c28bb78da8e6c0581ff0988b4142
GDamaso <guildamaso@gmail.com>
2024-05-13 11:18:00 -0700

    Fixed ESLint config with prettier

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -1,12 +1,11 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
+import eslintConfigPrettier from "eslint-config-prettier";
 
 export default [
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-  {
-    extends: ["prettier"],
-  },
+  eslintConfigPrettier,
 ];
