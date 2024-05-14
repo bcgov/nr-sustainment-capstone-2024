3c00ae4a36d37d7a32a6e6a897d15e0f6403f349
kcaparas1630 <kcaparas1630@gmail.com>
2024-05-14 12:12:33 -0700

    Changed tablename to developers and added mock data instead of developer's names

--- database/db/entrypoint/01-initseed.sql
+++ database/db/entrypoint/01-initseed.sql
@@ -1,11 +1,11 @@
-CREATE TABLE IF NOT EXISTS test (
+CREATE TABLE IF NOT EXISTS developers (
   ID INT PRIMARY KEY NOT NULL,
   FIRST_NAME VARCHAR(30) NOT NULL,
   LAST_NAME VARCHAR(30) NOT NULL
 );
 INSERT INTO
-  test (ID, FIRST_NAME, LAST_NAME)
+  developers (ID, FIRST_NAME, LAST_NAME)
 VALUES
-  (1, 'Kent', 'Caparas'),
-  (2, 'David', 'Goodfellows'),
-  (3, 'Damaso', 'Gui')
+  (1, 'John', 'Doe'),
+  (2, 'James', 'Doe'),
+  (3, 'Jim', 'Doe')
