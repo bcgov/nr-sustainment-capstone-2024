128bf38b389191c722237dcf522fcd8107d90f3c
kcaparas1630 <kcaparas1630@gmail.com>
2024-05-15 12:46:19 -0700

    added permission and user 1001 to dockerfile

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -26,7 +26,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Don't Click on the Vite and React logos to learn more
       </p>
     </>
   )
