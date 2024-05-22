9bf38fec229c842995ea6c4caab15c7fa5e6ed95
GDamaso <guildamaso@gmail.com>
2024-05-22 15:15:48 -0700

    Openshift PR deploy test

--- backend/src/controllers/developers-controller.ts
+++ backend/src/controllers/developers-controller.ts
@@ -9,6 +9,7 @@ const getDevelopers = async (req: Request, res: Response) => {
   const developers = await query('SELECT first_name, last_name FROM developers');
   const body = developers.rows.map((row: any) => row);
   res.status(200).send(body);
+  console.log("openshift test!");
 };
 
 export default getDevelopers;
