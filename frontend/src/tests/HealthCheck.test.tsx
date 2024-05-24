8885e325a251db5e66bcf951f9c1215f8f7f56c9
kcaparas1630 <kcaparas1630@gmail.com>
2024-05-23 17:12:16 -0700

    Added simple tests for HealthCheck component

---
+++ frontend/src/tests/HealthCheck.test.tsx
@@ -0,0 +1,16 @@
+import { expect, test } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import { waitFor } from '@testing-library/react';
+import HealthCheck from '../components/HealthCheck';
+
+test('HealthCheck heading', () => {
+  render(<HealthCheck />);
+  expect(screen.getByRole('heading', { level: 1, name: 'Health Check' })).toBeDefined();
+});
+
+test('HealthCheck paragraph stats', async () => {
+  render(<HealthCheck />);
+  await waitFor(() => {
+    expect(screen.getByRole('heading', { level: 2, name: 'NMP API is healthy and ready!' })).toBeDefined();
+  });
+});
