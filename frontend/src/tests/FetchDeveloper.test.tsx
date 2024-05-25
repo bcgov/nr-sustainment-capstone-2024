920a60b4ec0da0e86bbfb49f74ec8402ba381544
kcaparas1630 <kcaparas1630@gmail.com>
2024-05-24 20:11:02 -0700

    configured repetitive expect statements and used foreach to be DRY

--- frontend/src/tests/FetchDeveloper.test.tsx
+++ frontend/src/tests/FetchDeveloper.test.tsx
@@ -1,12 +1,11 @@
 import { render, screen, waitFor } from '@testing-library/react';
+// imports toBeInTheDocument
 import '@testing-library/jest-dom';
 import { vi, describe, it, expect } from 'vitest';
 import axios from 'axios';
 import FetchDevelopers from '../components/FetchDevelopers';
 
-// Mock the Axios module
 vi.mock('axios');
-
 describe('FetchDevelopers component', () => {
   it('fetches and displays developers data', async () => {
     // Mocked data returned by the Axios GET request
@@ -15,26 +14,18 @@ describe('FetchDevelopers component', () => {
       { id: 2, first_name: 'K', last_name: 'Caparas' },
       { id: 3, first_name: 'S', last_name: 'Spy' },
     ];
-
     // Setup the mock to return the mocked data
     vi.mocked(axios.get).mockResolvedValueOnce({ data: developers });
-
-    // Render the component
     render(<FetchDevelopers />);
-
     // Wait for the developers data to be fetched and displayed
     await waitFor(() => {
       // Check if the correct number of developer rows are displayed
       const rows = screen.getAllByRole('row');
-      // Check for developers.length
       expect(rows).toHaveLength(developers.length + 1);
-      // Check if specific developer data is displayed
-      expect(screen.getByText('K')).toBeInTheDocument();
-      expect(screen.getByText('Caparas')).toBeInTheDocument();
-      expect(screen.getByText('G')).toBeInTheDocument();
-      expect(screen.getByText('Damaso')).toBeInTheDocument();
-      expect(screen.getByText('S')).toBeInTheDocument();
-      expect(screen.getByText('Spy')).toBeInTheDocument();
+      developers.forEach((developer) => {
+        expect(screen.getByText(developer.first_name)).toBeInTheDocument();
+        expect(screen.getByText(developer.last_name)).toBeInTheDocument();
+      });
     });
   });
 });
--- frontend/src/tests/HealthCheck.test.tsx
+++ frontend/src/tests/HealthCheck.test.tsx
@@ -3,7 +3,6 @@ import { render, screen, waitFor } from '@testing-library/react';
 import axios from 'axios';
 import HealthCheck from '../components/HealthCheck';
 
-// Mock the Axios module
 vi.mock('axios');
 
 describe('HealthCheck Component', () => {
@@ -13,10 +12,7 @@ describe('HealthCheck Component', () => {
 
     // Setup the mock to return the mocked data
     vi.mocked(axios.get).mockResolvedValueOnce({ data: response });
-
-    // Render the component
     render(<HealthCheck />);
-
     // Wait for the healthcheck data to be fetched and displayed
     await waitFor(() => {
       expect(screen.getByText('NMP API is healthy and ready!')).toBeInTheDocument();
