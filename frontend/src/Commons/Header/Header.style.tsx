27570dc51df594a819e4070be22f3ebe50ddd05b
kcaparas1630 <kcaparas1630@gmail.com>
2024-06-03 18:20:18 -0700

    changes based on comments

--- frontend/src/Commons/Header/Header.style.tsx
+++ frontend/src/Commons/Header/Header.style.tsx
@@ -1,6 +1,8 @@
 import styled from '@emotion/styled';
 import * as tokens from '@bcgov/design-tokens/js';
-import { screenSizes } from '../../Constants/ScreenSize';
+import screenSizes from '../../Constants/ScreenSize';
+import desktopLogo from '../../../public/assets/BCID-logo-desktop.png';
+import mobileLogo from '../../../public/assets/BCID-logo-mobile.png';
 
 const StyledHeader = styled.header`
   display: flex;
@@ -11,7 +13,7 @@ const StyledHeader = styled.header`
   border-top-right-radius: 2px;
   flex-direction: column;
 
-  /* Tablet */
+
   @media (min-width: ${screenSizes.tablet}){
 
   }
@@ -27,21 +29,21 @@ const StyledHeader = styled.header`
 const Logo = styled.img`
   max-height: 112px;
   max-width: 123px;
-  content: url('../../../public/assets/BCID-logo-mobile.png');
+  content: url(${mobileLogo});
   order: 2; /* Moves logo below text */
 
-  /* Desktop */
+
   @media (min-width: ${screenSizes.desktop}){
     max-height: 123px;
     max-width: 319px;
-    content: url('../../../public/assets/BCID-logo-desktop.png');
+    content: url(${desktopLogo});
     order: 1; /* Moves Logo before text */
   }
 `;
 
 const Title = styled.h1`
   flex: 1;
-  font-family: ${tokens.typographyFontFamiliesBcSans};
+  font-family: ${tokens.typographyFontFamiliesBcSans}, sans-serif;
   font-weight: ${tokens.typographyFontWeightsBold};
   text-align: center;
   font-size: 32px;
@@ -49,7 +51,6 @@ const Title = styled.h1`
   margin: 50px 0 20px 0;
   line-height: 54px;
 
-  /* Desktop */
   @media (min-width: ${screenSizes.desktop}){
     order: 2; /* Moves text after logo */
     font-size: 48px;
--- frontend/src/Constants/ScreenSize.tsx
+++
@@ -1,7 +0,0 @@
-const screenSizes = {
-  mobile: '320px',
-  tablet: '768px',
-  desktop: '1024px',
-} as const;
-export { screenSizes };
-export type ScreenSizes = typeof screenSizes;
