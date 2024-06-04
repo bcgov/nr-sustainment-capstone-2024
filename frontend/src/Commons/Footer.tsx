74337c803eee1833d292fa15a33101305ee7679b
GDamaso <guildamaso@gmail.com>
2024-06-04 16:13:50 -0700

    Formatted and Linted

--- frontend/src/Commons/Footer.tsx
+++ frontend/src/Commons/Footer.tsx
@@ -2,10 +2,10 @@
  * @desc A footer with links to our terms of agreement and BC NMP Help resource
  *@author @GDamaso
  */
-import StyledFooter from './Styles/Footer.style.tsx';
+import StyledFooter from './Styles/Footer.style';
 
-const termsURL = "https://nmp.apps.nrs.gov.bc.ca/Information/Disclaimer";
-const helpURL = "https://www2.gov.bc.ca/gov/content/industry/agriculture-seafood/agricultural-land-and-environment/soil-nutrients/nutrient-management/nutrient-management-calculator/support-for-the-bc-nutrient-management-calculator";
+const termsURL = 'https://nmp.apps.nrs.gov.bc.ca/Information/Disclaimer';
+const helpURL = 'https://www2.gov.bc.ca/gov/content/industry/agriculture-seafood/agricultural-land-and-environment/soil-nutrients/nutrient-management/nutrient-management-calculator/support-for-the-bc-nutrient-management-calculator';
 
 const Footer = () => (
   <StyledFooter>
@@ -15,9 +15,7 @@ const Footer = () => (
     </p>
     <p>
       Need&nbsp;
-      <a href={helpURL}>
-        Help?
-      </a>
+      <a href={helpURL}>Help?</a>
     </p>
   </StyledFooter>
 );
