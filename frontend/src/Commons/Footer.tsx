a286c9dccfd4e4783f6187b1df3783c84b2c15cd
GDamaso <guildamaso@gmail.com>
2024-06-04 15:55:30 -0700

    Moved urls to constants

--- frontend/src/Commons/Footer.tsx
+++ frontend/src/Commons/Footer.tsx
@@ -4,15 +4,18 @@
  */
 import StyledFooter from './Styles/Footer.style.tsx';
 
+const termsURL = "https://nmp.apps.nrs.gov.bc.ca/Information/Disclaimer";
+const helpURL = "https://www2.gov.bc.ca/gov/content/industry/agriculture-seafood/agricultural-land-and-environment/soil-nutrients/nutrient-management/nutrient-management-calculator/support-for-the-bc-nutrient-management-calculator";
+
 const Footer = () => (
   <StyledFooter>
     <p>
       By continuing, you agree to our&nbsp;
-      <a href="https://nmp.apps.nrs.gov.bc.ca/Information/Disclaimer">Terms of Service</a>
+      <a href={termsURL}>Terms of Service</a>
     </p>
     <p>
       Need&nbsp;
-      <a href="https://www2.gov.bc.ca/gov/content/industry/agriculture-seafood/agricultural-land-and-environment/soil-nutrients/nutrient-management/nutrient-management-calculator/support-for-the-bc-nutrient-management-calculator">
+      <a href={helpURL}>
         Help?
       </a>
     </p>
