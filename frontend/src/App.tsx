6bfb02cb56b2e8f51b615cb0264fa3a8fe40d3f7
kcaparas1630 <kcaparas1630@gmail.com>
2024-06-05 10:22:01 -0700

    removed unused imports

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,5 @@
 import '@bcgov/design-tokens/css/variables.css';
 import '@bcgov/bc-sans/css/BC_Sans.css';
-import HealthEndpoint from './Views/HealthCheck';
-import FetchDevelopers from './Views/FetchDevelopers';
 import Header from './Commons/Header/Header';
 
 const App = () => (
