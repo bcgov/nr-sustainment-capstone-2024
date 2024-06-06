aecbaf1a56a909c854ef1b3410d4e5454edcd8d9
GDamaso <guildamaso@gmail.com>
2024-06-06 15:55:24 -0700

    Add first InputModule Farm Information

---
+++ frontend/src/Commons/Forms/InputModules/FarmInformation.tsx
@@ -0,0 +1,10 @@
+// import FormHeader from './FormHeader';
+// import StyledFormModule from '../Styles/FormModule.style';
+
+// const FormModule = () => (
+//   <StyledFormModule>
+//     <FormHeader text="Farm Information" active />
+//   </StyledFormModule>
+// );
+
+// export default FormModule;
---
+++ frontend/src/Commons/Forms/InputModules/Styles/FarmInformation.tsx
@@ -0,0 +1,9 @@
+// import FormHeader from '../FormHeader';
+
+// const FormModule = () => (
+//   <StyledFormModule>
+//     <FormHeader text="Farm Information" active />
+//   </StyledFormModule>
+// );
+
+// export default FormModule;
