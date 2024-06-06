98ff936a1faefdb0a39ab2a640240b4b38a89943
GDamaso <guildamaso@gmail.com>
2024-06-05 21:05:57 -0700

    Add optional `path: string` to Buttons.tsx

--- frontend/src/Commons/Button.tsx
+++ frontend/src/Commons/Button.tsx
@@ -1,12 +1,16 @@
+import { useNavigate } from 'react-router-dom';
 import StyledButton from './Styles/Button.style';
 
 type ButtonTypes = {
   text: string;
+  path?: string;
 };
 
-const Button = ({ text }: ButtonTypes) => {
+const Button = ({ text, path }: ButtonTypes) => {
+  const navigate = useNavigate();
+
   const handleClick = () => {
-    console.log('button clicked');
+    path ? navigate(path) : null;
   };
   return (
     <StyledButton type="button" onClick={handleClick}>
