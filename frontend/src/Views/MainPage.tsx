051313a8af7f98d70d86888892efd6c18b47caaa
GDamaso <guildamaso@gmail.com>
2024-06-06 15:54:44 -0700

    Add Main form page

--- frontend/src/Views/MainPage.tsx
+++ frontend/src/Views/MainPage.tsx
@@ -1,13 +1,15 @@
 import Header from '../Commons/Header';
 import Footer from '../Commons/Footer';
 import FormModule from '../Commons/Forms/FormModule';
+import StyledMain from './Styles/MainPage.styles';
+import Button from '../Commons/Button';
 
 const MainPage = () => (
-  <>
+  <StyledMain>
     <Header />
-    <FormModule />
+    <FormModule InputModule={() => <Button text="Hello world!" path="/" />} />
     <Footer />
-  </>
+  </StyledMain>
 );
 
 export default MainPage;
---
+++ frontend/src/Views/Styles/MainPage.styles.ts
@@ -0,0 +1,11 @@
+import styled from '@emotion/styled';
+import * as tokens from '@bcgov/design-tokens/js';
+// import screenSizes from '../../Constants/ScreenSize';
+
+const StyledMain = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background: ${tokens.themeBlue40};
+`;
+export default StyledMain;
