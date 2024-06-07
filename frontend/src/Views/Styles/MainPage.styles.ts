4d5c396e928177e4966755e9845117abe3dffdfb
GDamaso <guildamaso@gmail.com>
2024-06-06 21:41:50 -0700

    Main Page Styling
    Header Styling

--- frontend/src/Views/Styles/MainPage.styles.ts
+++ frontend/src/Views/Styles/MainPage.styles.ts
@@ -1,11 +1,11 @@
 import styled from '@emotion/styled';
-import * as tokens from '@bcgov/design-tokens/js';
+// import * as tokens from '@bcgov/design-tokens/js';
 // import screenSizes from '../../Constants/ScreenSize';
 
 const StyledMain = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  background: ${tokens.themeBlue40};
+  width:100%
 `;
 export default StyledMain;
