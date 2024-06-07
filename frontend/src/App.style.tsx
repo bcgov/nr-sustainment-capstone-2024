f6de36dd28956919ab48635c65c31e10b72ad9d4
GDamaso <guildamaso@gmail.com>
2024-06-07 14:59:14 -0700

    Added a styled flex container to App

---
+++ frontend/src/App.style.tsx
@@ -0,0 +1,11 @@
+import styled from '@emotion/styled';
+// import * as tokens from '@bcgov/design-tokens/js';
+// import screenSizes from '../../Constants/ScreenSize';
+
+const StyledApp = styled.div`
+display: flex;
+flex-direction: column;
+min-height: 100vh;
+`
+
+export default StyledApp;
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,16 @@
 import '@bcgov/design-tokens/css/variables.css';
 import '@bcgov/bc-sans/css/BC_Sans.css';
+import StyledApp from './App.style';
 import LandingPage from './Views/LandingPage';
 import Header from './Commons/Header';
 import Footer from './Commons/Footer';
 
 const App = () => (
-  <>
+  <StyledApp>
     <Header />
     <LandingPage />
     <Footer />
-  </>
+  </StyledApp>
 );
 
 export default App;
