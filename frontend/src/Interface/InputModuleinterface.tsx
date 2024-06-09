b8058e27d87449885241f991bfad386fa503b112
GDamaso <guildamaso@gmail.com>
2024-06-08 20:06:27 -0700

    Fixed default export semicolon shannanigan

--- frontend/src/Interface/InputModuleinterface.tsx
+++ frontend/src/Interface/InputModuleinterface.tsx
@@ -1,10 +1,11 @@
 import { IconDefinition } from '@fortawesome/free-solid-svg-icons';
 import React from 'react';
 
-export default interface InputModuleInterface {
+interface InputModuleInterface {
   InputModuleComponent: React.ComponentType<{ className?: string }>;
   id: string;
   name: string;
   faIcon: IconDefinition;
   enable: boolean;
-};
+}
+export default InputModuleInterface;
