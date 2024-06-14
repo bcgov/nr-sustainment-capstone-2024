c14e66dba34b464f9d4b640ff4474e29606af6d1
GDamaso <guildamaso@gmail.com>
2024-06-14 16:59:52 -0700

    Add CustomSelect reusable component

---
+++ frontend/src/Commons/Input/Select/CustomSelect.style.ts
@@ -0,0 +1,33 @@
+/**
+ * @desc Styling with BC Design Tokens and Emotion for Reusable Select Dropdown component
+ * @author @GDamaso
+ */
+import styled from '@emotion/styled';
+import * as tokens from '@bcgov/design-tokens/js';
+import screenSizes from '@Constants/ScreenSize';
+
+type StyledSelectProps = {
+  width: string;
+};
+
+const StyledSelect = styled.div<StyledSelectProps>`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  label {
+    font-weight: ${tokens.typographyFontWeightsBold};
+  }
+
+  select {
+    border: solid 1px ${tokens.themeGray40};
+    border-radius: 3px;
+    height: 30px;
+    background-color: ${tokens.themeGrayWhite};
+  }
+
+  @media (min-width: ${screenSizes.desktop}) {
+    width: ${(props) => props.width};
+  }
+`;
+
+export default StyledSelect;
