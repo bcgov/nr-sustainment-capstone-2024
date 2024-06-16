22edeb14a954c68e270a6b314dfc153c555383f8
GDamaso <guildamaso@gmail.com>
2024-06-15 20:25:29 -0700

    Simple Summary for testing

--- frontend/src/Commons/Forms/InputModules/Summary/Summary.tsx
+++ frontend/src/Commons/Forms/InputModules/Summary/Summary.tsx
@@ -7,53 +7,34 @@
  * @description A module with input fields to be used in a Form Module
  * @author @GDamaso
  */
-import Button from '@Commons/Button/Button.tsx';
 import InputModuleInterface from 'src/Interface/InputModuleinterface';
 import { faList } from '@fortawesome/free-solid-svg-icons';
-import { StyledFarmInfo, StyledButtonContainer } from './Summary.style';
+import InputModuleProps from 'src/Interface/InputModuleProps';
+import { StyledFarmInfo } from './Summary.style';
 
-const FarmInfoComponent = () => (
+const SummaryComponent: React.FC<InputModuleProps> = ({ farmDetails }) => (
   <StyledFarmInfo>
-    <label htmlFor="farmName">
-      Name
-      <br />
-      <input
-        type="text"
-        name="farmName"
-      />
-    </label>
-    <label htmlFor="farmYear">
-      Year
-      <br />
-      <input
-        type="text"
-        name="farmYear"
-      />
-    </label>
-    <label htmlFor="farmRegion">
-      Region
-      <br />
-      {/* <p> Region selection will customize recommendations to your local climate. </p> */}
-      <StyledButtonContainer>
-        <select
-          id="farmRegion"
-          name="farmRegion"
-        >
-          <option value="Vancouver Island">Vancouver Island </option>
-        </select>
-        <Button
-          text="Next"
-          size="sm"
-          disabled={false}
-          path="/"
-        />
-      </StyledButtonContainer>
-    </label>
+    <p>
+      Farm Name:&nbsp;
+      {farmDetails.FarmName}
+    </p>
+    <p>
+      Year:&nbsp;
+      {farmDetails.Year}
+    </p>
+    <p>
+      Region:&nbsp;
+      {farmDetails.FarmRegion}
+    </p>
+    <p>
+      Berries:&nbsp;
+      {farmDetails.HasBerries}
+    </p>
   </StyledFarmInfo>
 );
 
 const Summary: InputModuleInterface = {
-  InputModuleComponent: FarmInfoComponent,
+  InputModuleComponent: SummaryComponent,
   id: 'Summary',
   name: { long: 'Summary', short: 'Summary' },
   faIcon: faList,
