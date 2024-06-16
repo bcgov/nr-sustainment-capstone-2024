1517d816a89dfba85488873aab8b786044c0262a
GDamaso <guildamaso@gmail.com>
2024-06-15 20:24:08 -0700

    Simplify MainPage IModules map

--- frontend/src/Views/MainPage/MainPage.tsx
+++ frontend/src/Views/MainPage/MainPage.tsx
@@ -7,20 +7,20 @@ import MainPageHeader from '@Commons/MainPageHeader/MainPageHeader';
 import ProgressBar from '@Commons/ProgressBar/ProgressBar';
 import MainPageFooter from '@Commons/MainPageFooter/MainPageFooter';
 import FormModule from '@Commons/Forms/FormModule/FormModule.tsx';
-import * as inputModules from '@Commons/Forms/InputModules/index';
 import InputModuleInterface from 'src/Interface/InputModuleinterface';
 import FarmDetailsInterface from 'src/Interface/FarmDetailsInterface';
 import { StyledMain, StyledMainContainer } from './MainPage.styles';
+import * as InputModules from '@Commons/Forms/InputModules/index';
 
 // The sequence of sections to show up on the main page
 // This is the skeleton for the Berries workflow
 // Uncomment sections as they are implemented to have them instantiated ;)
-const mockBerriesWorkflow = [
-  { InputModuleID: 'FarmInformation' },
-  { InputModuleID: 'FieldsAndSoil' },
-  { InputModuleID: 'ManureAndCompost' },
-  { InputModuleID: 'Calculate' },
-  { InputModuleID: 'Summary' },
+const mockBerriesWorkflow: InputModuleInterface[] = [
+  InputModules.FarmInformation,
+  InputModules.FieldsAndSoil,
+  InputModules.ManureAndCompost,
+  InputModules.Calculate,
+  InputModules.Summary,
 ];
 
 // Initial Values for calculation, some defaults are being used
@@ -43,18 +43,14 @@ const MainPage: React.FC = () => {
       <MainPageHeader />
       <ProgressBar />
       <StyledMainContainer>
-        {mockBerriesWorkflow.map((formSection) => {
-          // Not sure what type to use for `inputModules` here
-          const InputModule: InputModuleInterface = (inputModules as any)[
-            formSection.InputModuleID
-          ];
+        {mockBerriesWorkflow.map((InputModule) => {
           if (InputModule) {
             return (
               <FormModule
                 InputModule={InputModule}
                 farmDetails={farmDetails}
                 updateFarmDetails={updateFarmDetails}
-                key={formSection.InputModuleID}
+                key={InputModule.id}
               />
             );
           }
