368ec8c5bc6022de54682ede57ae7c69f256d7f7
GDamaso <guildamaso@gmail.com>
2024-06-15 20:24:53 -0700

    Add Button Types Interface

---
+++ frontend/src/Interface/ButtonTypes.tsx
@@ -0,0 +1,8 @@
+/**
+ * @desc   A simple OptionInterface to define option objects for a
+ *         <select> component
+ * @author @GDamaso
+ */
+type ButtonTypes = 'button' | 'submit' | 'reset' | undefined;
+
+export default ButtonTypes;
