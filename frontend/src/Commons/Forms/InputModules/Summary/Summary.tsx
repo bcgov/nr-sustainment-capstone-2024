15523d5586f4c444e0ce118d7dec432cf19e6eb7
kcaparas1630 <kcaparas1630@gmail.com>
2024-06-17 10:55:37 -0700

    removed proptypes, don't need anymore

--- frontend/src/Commons/Forms/InputModules/Summary/Summary.tsx
+++ frontend/src/Commons/Forms/InputModules/Summary/Summary.tsx
@@ -10,7 +10,6 @@
 import React from 'react';
 import InputModuleProps from 'src/Interface/InputModuleProps';
 import InputModuleInterface from 'src/Interface/InputModuleinterface';
-import PropTypes from 'prop-types';
 import { faList } from '@fortawesome/free-solid-svg-icons';
 import Button from '@Commons/Button/Button';
 import { StyledFarmInfo } from './Summary.style';
@@ -47,14 +46,14 @@ const SummaryComponent: React.FC<InputModuleProps> = ({ farmDetails }) => (
 isRequired returns a warning if the prop isn't provided.
 PropTypes.shape is useful for key-value pairs. Such as below.
 */
-SummaryComponent.propTypes = {
-  farmDetails: PropTypes.shape({
-    FarmName: PropTypes.string.isRequired,
-    Year: PropTypes.string.isRequired,
-    FarmRegion: PropTypes.string.isRequired,
-    HasBerries: PropTypes.bool.isRequired,
-  }).isRequired,
-};
+// SummaryComponent.propTypes = {
+//   farmDetails: PropTypes.shape({
+//     FarmName: PropTypes.string.isRequired,
+//     Year: PropTypes.string.isRequired,
+//     FarmRegion: PropTypes.string.isRequired,
+//     HasBerries: PropTypes.bool.isRequired,
+//   }).isRequired,
+// };
 
 const Summary: InputModuleInterface = {
   InputModuleComponent: SummaryComponent,
