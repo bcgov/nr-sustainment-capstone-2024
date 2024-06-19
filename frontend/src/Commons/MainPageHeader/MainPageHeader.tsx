bd3e36a9d6a09e9a3ee276c87e1e6015485d4e93
GDamaso <guildamaso@gmail.com>
2024-06-19 11:49:23 -0700

    Add a link to our logo, improve my life

--- frontend/src/Commons/MainPageHeader/MainPageHeader.tsx
+++ frontend/src/Commons/MainPageHeader/MainPageHeader.tsx
@@ -2,7 +2,9 @@ import { StyledHeader, Logo, Title } from './MainPageHeader.styles';
 
 const MainPageHeader = () => (
   <StyledHeader>
-    <Logo alt="bc-logo" />
+    <a href="/">
+      <Logo alt="bc-logo" />
+    </a>
     <Title>Better Berries</Title>
   </StyledHeader>
 );
