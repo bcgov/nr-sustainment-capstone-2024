5c2da1682c540c9fafeabef99cefa33f56d3a75b
GDamaso <guildamaso@gmail.com>
2024-06-25 16:03:26 -0700

    Fixed CustomLink wrong padding

--- frontend/src/Commons/CustomLink/CustomLink.style.ts
+++ frontend/src/Commons/CustomLink/CustomLink.style.ts
@@ -21,7 +21,6 @@ const StyledLinkContainer = styled.div<StyledLinkProps>`
   border: ${`1px solid ${tokens.surfaceColorBorderMedium}`};
   font-family: ${tokens.typographyFontFamiliesBcSans};
   font-weight: ${tokens.typographyFontWeightsBold};
-  padding: 20px 30px;
   @media (min-width: ${screenSizes.desktop}) {
     height: 100%;
     max-width: ${(props) => getButtonSize(props.size, true)};
@@ -32,12 +31,11 @@ const StyledLinkContainer = styled.div<StyledLinkProps>`
     align-items: center;
     justify-content: center;
     color: inherit;
+    padding: 20px 30px;
     text-decoration: none;
     height: 100%;
     width: 100%;
     text-align: center;
-    max-height: ${(props) => (props.size === 'sm' ? '27px' : props.size === 'md' ? '25px' : '41.6px')};
-    max-width: ${(props) => (props.size === 'sm' ? '52px' : props.size === 'md' ? '200px' : '301.6px')};
   }
 `;
 
