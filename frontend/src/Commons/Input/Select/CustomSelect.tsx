ead39a729ed31b494a1c97da4669d732cdcd372d
kcaparas1630 <kcaparas1630@gmail.com>
2024-07-07 21:08:37 -0700

    Reverted back css file import

--- frontend/src/Commons/Input/Select/CustomSelect.tsx
+++ frontend/src/Commons/Input/Select/CustomSelect.tsx
@@ -6,6 +6,7 @@ import { Field, ErrorMessage } from 'formik';
 import { FC } from 'react';
 import OptionInterface from 'src/Interface/OptionInterface';
 import StyledSelect from './CustomSelect.style';
+import '../ErrorMessage.css';
 
 interface CustomSelectProps {
   label: string;
