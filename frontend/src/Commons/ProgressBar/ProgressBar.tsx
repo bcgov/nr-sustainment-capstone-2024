7be6bab59288566be7cc5865bf0e6147eff447fc
GDamaso <guildamaso@gmail.com>
2024-07-08 11:40:15 -0700

    Cleanup

--- frontend/src/Commons/ProgressBar/ProgressBar.tsx
+++ frontend/src/Commons/ProgressBar/ProgressBar.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+// import React, { useEffect } from 'react';
 import InputModuleInterface from 'src/Interface/InputModuleinterface';
 import ProgressItem from '../ProgressItem/ProgressItem';
 import { Container, StyledProgressBar, StyledLineBlock } from './ProgressBar.styles';
@@ -7,26 +7,18 @@ interface ProgressBarProps {
   formStates: InputModuleInterface[];
 }
 
-const ProgressBar: React.FC<ProgressBarProps> = ({ formStates }) => {
-  useEffect(() => {
-    if (formStates) {
-      console.log('PG State');
-    }
-  }, [formStates]);
-
-  return (
-    <Container>
-      <StyledProgressBar>
-        {formStates.map((progressItem) => (
-          <ProgressItem
-            InputModule={progressItem}
-            key={progressItem.id}
-          />
-        ))}
-      </StyledProgressBar>
-      <StyledLineBlock />
-    </Container>
-  );
-};
+const ProgressBar: React.FC<ProgressBarProps> = ({ formStates }) => (
+  <Container>
+    <StyledProgressBar>
+      {formStates.map((progressItem) => (
+        <ProgressItem
+          InputModule={progressItem}
+          key={progressItem.id}
+        />
+      ))}
+    </StyledProgressBar>
+    <StyledLineBlock />
+  </Container>
+);
 
 export default ProgressBar;
