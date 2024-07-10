9a7667e6a0be215716780a95565b592585c7b2c8
GDamaso <guildamaso@gmail.com>
2024-07-09 19:17:04 -0700

    Fix typo

--- frontend/src/Constants/EmptyFieldDetails.ts
+++ frontend/src/Constants/EmptyFieldDetails.ts
@@ -5,9 +5,9 @@ const emptyFieldDetails: FieldDetailInterface = {
   FieldName: '',
   Area: 0,
   Comment: '',
-  hasSoilTest: null,
-  hasLeafTest: null,
-  SoilTest: { TestingMethod: '', sampleDate: '', valNO3H: 0, valP: 0, valK: 0, valPH: 0 },
+  HasSoilTest: null,
+  HasLeafTest: null,
+  SoilTest: { TestingMethod: '', sampleDate: '', valNO3H: 0, ValP: 0, valK: 0, valPH: 0 },
   LeafTest: { leafTissueP: 0, leafTissueK: 0 },
 };
 
