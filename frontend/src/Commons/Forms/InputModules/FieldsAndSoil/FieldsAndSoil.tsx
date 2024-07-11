9cad455cefb62ea15af9b1a9c44f428818cae3de
kcaparas1630 <kcaparas1630@gmail.com>
2024-07-11 14:40:01 -0700

    format

--- frontend/src/Commons/Forms/InputModules/FieldsAndSoil/FieldsAndSoil.tsx
+++ frontend/src/Commons/Forms/InputModules/FieldsAndSoil/FieldsAndSoil.tsx
@@ -68,30 +68,14 @@ const FieldsAndSoilComponent: FC<InputModuleProps> = ({
     Comment: Yup.string().max(200, 'Comments should be lower than 200 chars'),
     hasSoilTest: Yup.boolean().nullable().required('A Soil Test must be either `Yes` or `No`'),
     hasLeafTest: Yup.boolean().nullable().required('A Leaf Test must be either `Yes` or `No`'),
-    TestingMethod: Yup.string().when('hasSoilTest', (hasSoilTest) =>
-      hasSoilTest ? Yup.string().notRequired() : Yup.string().required('Must enter Testing Method'),
-    ),
-    sampleDate: Yup.string().when('hasSoilTest', (hasSoilTest) =>
-      hasSoilTest ? Yup.string().notRequired() : Yup.string().required('Must enter Sample Date'),
-    ),
-    valNO3H: Yup.number().when('hasSoilTest', (hasSoilTest) =>
-      hasSoilTest ? Yup.number().notRequired() : Yup.number().required('Required'),
-    ),
-    valP: Yup.number().when('hasSoilTest', (hasSoilTest) =>
-      hasSoilTest ? Yup.number().notRequired() : Yup.number().required('Required'),
-    ),
-    valK: Yup.number().when('hasSoilTest', (hasSoilTest) =>
-      hasSoilTest ? Yup.number().notRequired() : Yup.number().required('Required'),
-    ),
-    valPH: Yup.number().when('hasSoilTest', (hasSoilTest) =>
-      hasSoilTest ? Yup.number().notRequired() : Yup.number().required('Required'),
-    ),
-    leafTissueP: Yup.number().when('hasLeafTest', (hasLeafTest) =>
-      hasLeafTest ? Yup.number().notRequired() : Yup.number().required('Required'),
-    ),
-    leafTissueK: Yup.number().when('hasLeafTest', (hasLeafTest) =>
-      hasLeafTest ? Yup.number().notRequired() : Yup.number().required('Required'),
-    ),
+    TestingMethod: Yup.string().when('hasSoilTest', (hasSoilTest) => (hasSoilTest ? Yup.string().notRequired() : Yup.string().required('Must enter Testing Method'))),
+    sampleDate: Yup.string().when('hasSoilTest', (hasSoilTest) => (hasSoilTest ? Yup.string().notRequired() : Yup.string().required('Must enter Sample Date'))),
+    valNO3H: Yup.number().when('hasSoilTest', (hasSoilTest) => (hasSoilTest ? Yup.number().notRequired() : Yup.number().required('Required'))),
+    valP: Yup.number().when('hasSoilTest', (hasSoilTest) => (hasSoilTest ? Yup.number().notRequired() : Yup.number().required('Required'))),
+    valK: Yup.number().when('hasSoilTest', (hasSoilTest) => (hasSoilTest ? Yup.number().notRequired() : Yup.number().required('Required'))),
+    valPH: Yup.number().when('hasSoilTest', (hasSoilTest) => (hasSoilTest ? Yup.number().notRequired() : Yup.number().required('Required'))),
+    leafTissueP: Yup.number().when('hasLeafTest', (hasLeafTest) => (hasLeafTest ? Yup.number().notRequired() : Yup.number().required('Required'))),
+    leafTissueK: Yup.number().when('hasLeafTest', (hasLeafTest) => (hasLeafTest ? Yup.number().notRequired() : Yup.number().required('Required'))),
   });
 
   /**
