a066be7f15a670ae36f71fe8e0c9022ecb3a9cf8
kcaparas1630 <kcaparas1630@gmail.com>
2024-07-15 11:16:06 -0700

    comments on temp interface

--- frontend/src/Interface/NutrientsInterface.tsx
+++ frontend/src/Interface/NutrientsInterface.tsx
@@ -1,18 +1,18 @@
 export interface NutrientsInterface {
-    id: number,
-    fertilizerTypeId: string,
-    fertilizerId: string,
-    applRate: number,
-    applDate?: Date,
-    applMethodId?: string,
-    customN: number,
-    customP2o5: number,
-    customK2o: number,
-    fertN: number,
-    fertP2o5: number,
-    fertK2o: number,
-    liquidDensity: number,
-    liquidDensityUnitId: string,
+  id: number;
+  fertilizerTypeId: string;
+  fertilizerId: string;
+  applRate: number;
+  applDate?: Date;
+  applMethodId?: string;
+  customN: number;
+  customP2o5: number;
+  customK2o: number;
+  fertN: number;
+  fertP2o5: number;
+  fertK2o: number;
+  liquidDensity: number;
+  liquidDensityUnitId: string;
 }
-
-export interface TempNutrientsInterface extends NutrientsInterface{}
+// temporarily stored until it gets assigned to field
+export interface TempNutrientsInterface extends NutrientsInterface {}
