33b3a8e4a6afc302c000c352a5f1fa0f12ff7c8f
GDamaso <guildamaso@gmail.com>
2024-07-30 11:19:32 -0700

    Adding icon colors and small fixes

--- frontend/src/Commons/Forms/InputModules/CalculateNutrients/CalculateNutrientsList.tsx
+++ frontend/src/Commons/Forms/InputModules/CalculateNutrients/CalculateNutrientsList.tsx
@@ -1,11 +1,6 @@
-import { FC, useState, useEffect } from 'react';
-import FarmDetailsInterface from '@Interface/FarmDetailsInterface';
+import { FC } from 'react';
 import CropsDetailsInterface from '@Interface/CropsDetailsInterface';
 import FertilizerInterface from '@Interface/FertilizerInterface';
-import Calculate from '@Utils/Calculate/Calculate';
-import MainBalanceInterface from '@Interface/MainBalanceInterface';
-import CropRemovalBalanceInterface from '@Interface/CropRemovalBalance';
-import AgronomicBalanceInterface from '@Interface/AgronomicBalanceInterface';
 import { StyledDivider } from '../ListComponent.styles';
 import { faCircleCheck, faTriangleExclamation } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -19,65 +14,26 @@ import {
   StyledPItemList,
   StyledPContainer,
   DesktopView,
-  MobileView,
 } from './CalculateNutrientsList.styles';
+import MainBalanceInterface from '@Interface/MainBalanceInterface';
+import FieldDetailInterface from '@Interface/FieldDetailsInterface';
 
 interface CalculationListProps {
-  farmDetails: FarmDetailsInterface;
-  selectedFieldIndex: number;
-  hasFertilizerAdded: boolean;
+  field: FieldDetailInterface;
+  cropBalances: MainBalanceInterface[];
+  resultBalance: MainBalanceInterface;
 }
-
-const initialAgronomicBalance: AgronomicBalanceInterface = { N: 0, P: 0, K: 0 };
-const initialCropRemovalBalance: CropRemovalBalanceInterface = { P: 0, K: 0 };
-const initialBalance: MainBalanceInterface = {
-  agronomic: initialAgronomicBalance,
-  cropRemoval: initialCropRemovalBalance,
-};
 
 const zeroConstant: number = 0;
 
-const CalculationList: FC<CalculationListProps> = ({
-  farmDetails,
-  selectedFieldIndex,
-  hasFertilizerAdded,
-}) => {
-  const [cropBalance, setCropBalance] = useState<MainBalanceInterface>(initialBalance);
-  const [resultBalance, setResultBalance] = useState<MainBalanceInterface>(initialBalance);
+const CalculationList: FC<CalculationListProps> = ({ field, cropBalances, resultBalance }) => {
+  const getResultsIcon = (nutrient: number) => {
+    return nutrient >= 0 ? faCircleCheck : faTriangleExclamation;
+  };
 
-  useEffect(() => {
-    const field = farmDetails.Fields[selectedFieldIndex];
-    if (field) {
-      const balances = field.Crops.map((crop) => Calculate(field, crop));
-      const newCropBalance = balances[balances.length - 1];
-      setCropBalance(newCropBalance);
-
-      let fertN = 0;
-      let fertP = 0;
-      let fertK = 0;
-
-      if (hasFertilizerAdded && field.Nutrients.length > 0) {
-        field.Nutrients.forEach((fertilizer) => {
-          fertN += fertilizer.fertN;
-          fertP += fertilizer.fertP2o5;
-          fertK += fertilizer.fertK2o;
-        });
-      }
-
-      setResultBalance({
-        agronomic: {
-          N: newCropBalance.agronomic.N + fertN,
-          P: newCropBalance.agronomic.P + fertP,
-          K: newCropBalance.agronomic.K + fertK,
-        },
-        cropRemoval: {
-          N: newCropBalance.cropRemoval.N ? newCropBalance.cropRemoval.N + fertN : fertN,
-          P: newCropBalance.cropRemoval.P + fertP,
-          K: newCropBalance.cropRemoval.K + fertK,
-        },
-      });
-    }
-  }, [farmDetails.Fields[selectedFieldIndex], selectedFieldIndex, hasFertilizerAdded]);
+  const getResultIconColor = (nutrient: number) => {
+    return nutrient >= 0 ? '#42814A' : '#F8BB47';
+  };
 
   return (
     <>
@@ -115,7 +71,7 @@ const CalculationList: FC<CalculationListProps> = ({
           </StyledH4HeaderItem>
         </StyledH4HeaderContainer>
 
-        {farmDetails.Fields[selectedFieldIndex]?.Crops.map((crop: CropsDetailsInterface) => {
+        {field.Crops.map((crop: CropsDetailsInterface, index: number) => {
           return (
             <StyledPContainer key={`${crop}`}>
               <StyledPItem width="30%">
@@ -123,54 +79,52 @@ const CalculationList: FC<CalculationListProps> = ({
               </StyledPItem>
               <StyledPItem width="30%">
                 <StyledPItemList>
-                  <p>{cropBalance.agronomic.N}</p>
-                  <p>{cropBalance.agronomic.P}</p>
-                  <p>{cropBalance.agronomic.K}</p>
+                  <p>{cropBalances[index].agronomic.N}</p>
+                  <p>{cropBalances[index].agronomic.P}</p>
+                  <p>{cropBalances[index].agronomic.K}</p>
                 </StyledPItemList>
               </StyledPItem>
               <StyledPItem width="30%">
                 <StyledPItemList>
-                  <p>{zeroConstant}</p>
-                  <p>{cropBalance.cropRemoval.P}</p>
-                  <p>{cropBalance.cropRemoval.K}</p>
+                  <p>{cropBalances[index].cropRemoval.N || zeroConstant}</p>
+                  <p>{cropBalances[index].cropRemoval.P}</p>
+                  <p>{cropBalances[index].cropRemoval.K}</p>
                 </StyledPItemList>
               </StyledPItem>
             </StyledPContainer>
           );
         })}
 
-        {hasFertilizerAdded && farmDetails.Fields[selectedFieldIndex].Nutrients.length > 0 && (
+        {field.Nutrients.length > 0 && (
           <>
             <StyledH4HeaderItem width="30%">
               <h4>Fertilizer</h4>
             </StyledH4HeaderItem>
-            {farmDetails.Fields[selectedFieldIndex]?.Nutrients.map(
-              (fertilizer: FertilizerInterface) => {
-                return (
-                  <StyledPContainer
-                    key={`Fertilizer-${fertilizer.id}-${fertilizer.fertilizerId}-${farmDetails.Fields[selectedFieldIndex].Nutrients.length}`}
-                  >
-                    <StyledPItem width="30%">
-                      <p>{fertilizer.fertilizerId}</p>
-                    </StyledPItem>
-                    <StyledPItem width="30%">
-                      <StyledPItemList>
-                        <p>{fertilizer.fertN}</p>
-                        <p>{fertilizer.fertP2o5}</p>
-                        <p>{fertilizer.fertK2o}</p>
-                      </StyledPItemList>
-                    </StyledPItem>
-                    <StyledPItem width="30%">
-                      <StyledPItemList>
-                        <p>{fertilizer.fertN}</p>
-                        <p>{fertilizer.fertP2o5}</p>
-                        <p>{fertilizer.fertK2o}</p>
-                      </StyledPItemList>
-                    </StyledPItem>
-                  </StyledPContainer>
-                );
-              },
-            )}
+            {field.Nutrients.map((fertilizer: FertilizerInterface, index) => {
+              return (
+                <StyledPContainer
+                  key={`Fertilizer-${fertilizer.id}-${fertilizer.fertilizerId}-${index}`}
+                >
+                  <StyledPItem width="30%">
+                    <p>{fertilizer.fertilizerId}</p>
+                  </StyledPItem>
+                  <StyledPItem width="30%">
+                    <StyledPItemList>
+                      <p>{fertilizer.fertN}</p>
+                      <p>{fertilizer.fertP2o5}</p>
+                      <p>{fertilizer.fertK2o}</p>
+                    </StyledPItemList>
+                  </StyledPItem>
+                  <StyledPItem width="30%">
+                    <StyledPItemList>
+                      <p>{fertilizer.fertN}</p>
+                      <p>{fertilizer.fertP2o5}</p>
+                      <p>{fertilizer.fertK2o}</p>
+                    </StyledPItemList>
+                  </StyledPItem>
+                </StyledPContainer>
+              );
+            })}
           </>
         )}
         <StyledDivider />
@@ -182,19 +136,22 @@ const CalculationList: FC<CalculationListProps> = ({
             <StyledPItemList>
               <p>
                 <FontAwesomeIcon
-                  icon={resultBalance.agronomic.N > 0 ? faCircleCheck : faTriangleExclamation}
+                  icon={getResultsIcon(resultBalance.agronomic.N)}
+                  color={getResultIconColor(resultBalance.agronomic.N)}
                 />
                 {resultBalance.agronomic.N}
               </p>
               <p>
                 <FontAwesomeIcon
-                  icon={resultBalance.agronomic.N > 0 ? faCircleCheck : faTriangleExclamation}
+                  icon={getResultsIcon(resultBalance.agronomic.P)}
+                  color={getResultIconColor(resultBalance.agronomic.P)}
                 />
                 {resultBalance.agronomic.P}
               </p>
               <p>
                 <FontAwesomeIcon
-                  icon={resultBalance.agronomic.N > 0 ? faCircleCheck : faTriangleExclamation}
+                  icon={getResultsIcon(resultBalance.agronomic.K)}
+                  color={getResultIconColor(resultBalance.agronomic.K)}
                 />
                 {resultBalance.agronomic.K}
               </p>
@@ -202,164 +159,31 @@ const CalculationList: FC<CalculationListProps> = ({
           </StyledPItem>
           <StyledPItem width="30%">
             <StyledPItemList>
-              <p>{resultBalance.cropRemoval.N}</p>
-              <p>{resultBalance.cropRemoval.P}</p>
-              <p>{resultBalance.cropRemoval.K}</p>
+              <p>
+                <FontAwesomeIcon
+                  icon={getResultsIcon(resultBalance.cropRemoval.N || 0)}
+                  color={getResultIconColor(resultBalance.cropRemoval.N || 0)}
+                />
+                {resultBalance.cropRemoval.N || 0}
+              </p>
+              <p>
+                <FontAwesomeIcon
+                  icon={getResultsIcon(resultBalance.cropRemoval.P)}
+                  color={getResultIconColor(resultBalance.cropRemoval.P)}
+                />
+                {resultBalance.cropRemoval.P}
+              </p>
+              <p>
+                <FontAwesomeIcon
+                  icon={getResultsIcon(resultBalance.cropRemoval.K)}
+                  color={getResultIconColor(resultBalance.cropRemoval.K)}
+                />
+                {resultBalance.cropRemoval.K}
+              </p>
             </StyledPItemList>
           </StyledPItem>
         </StyledPContainer>
       </DesktopView>
-
-      <MobileView>
-        <>
-          <StyledH3HeaderItem width="100%">
-            <h3>Agronomic (lb/ac)</h3>
-          </StyledH3HeaderItem>
-          <StyledH4HeaderContainer>
-            <StyledH4HeaderItem width="30%">
-              <h4>Crop</h4>
-            </StyledH4HeaderItem>
-            <StyledH4HeaderItem width="30%">
-              <StyledH4HeaderList>
-                <h4>N</h4>
-                <h4>P2O5</h4>
-                <h4>K2O</h4>
-              </StyledH4HeaderList>
-            </StyledH4HeaderItem>
-          </StyledH4HeaderContainer>
-          {farmDetails.Fields[selectedFieldIndex]?.Crops.map((Crop: CropsDetailsInterface) => {
-            const { cropId } = Crop;
-
-            return (
-              <StyledPContainer key={cropId}>
-                <StyledPItem width="30%">
-                  <p>{cropId === '75' ? 'Blueberry' : 'Raspberry'}</p>
-                </StyledPItem>
-                <StyledPItem width="30%">
-                  <StyledPItemList>
-                    <p>{zeroConstant}</p>
-                    <p>{zeroConstant}</p>
-                    <p>{zeroConstant}</p>
-                  </StyledPItemList>
-                </StyledPItem>
-              </StyledPContainer>
-            );
-          })}
-          {hasFertilizerAdded && farmDetails.Fields[selectedFieldIndex].Nutrients.length > 0 && (
-            <>
-              <StyledH4HeaderItem width="30%">
-                <h4>Fertilizer</h4>
-              </StyledH4HeaderItem>
-              {farmDetails.Fields[selectedFieldIndex]?.Nutrients.map(
-                (fertilizer: FertilizerInterface) => {
-                  const { fertilizerId } = fertilizer;
-                  return (
-                    <StyledPContainer key={`Fertilizer-${fertilizer.id}-${fertilizerId}`}>
-                      <StyledPItem width="30%">
-                        <p>{fertilizerId}</p>
-                      </StyledPItem>
-                      <StyledPItem width="30%">
-                        <StyledPItemList>
-                          <p>{zeroConstant}</p>
-                          <p>{zeroConstant}</p>
-                          <p>{zeroConstant}</p>
-                        </StyledPItemList>
-                      </StyledPItem>
-                    </StyledPContainer>
-                  );
-                },
-              )}
-            </>
-          )}
-          <StyledDivider />
-
-          <StyledPContainer>
-            <StyledPItem width="30%">
-              <p>Balance</p>
-            </StyledPItem>
-            <StyledPItem width="30%">
-              <StyledPItemList>
-                <p>{zeroConstant}</p>
-                <p>{zeroConstant}</p>
-                <p>{zeroConstant}</p>
-              </StyledPItemList>
-            </StyledPItem>
-          </StyledPContainer>
-          <StyledH3HeaderItem width="100%">
-            <h3>Crop Removal (lb/ac)</h3>
-          </StyledH3HeaderItem>
-          <StyledH4HeaderContainer>
-            <StyledH4HeaderItem width="30%">
-              <h4>Crop</h4>
-            </StyledH4HeaderItem>
-            <StyledH4HeaderItem width="30%">
-              <StyledH4HeaderList>
-                <h4>N</h4>
-                <h4>P2O5</h4>
-                <h4>K2O</h4>
-              </StyledH4HeaderList>
-            </StyledH4HeaderItem>
-          </StyledH4HeaderContainer>
-          {farmDetails.Fields[selectedFieldIndex]?.Crops.map((Crop: CropsDetailsInterface) => {
-            const { cropId } = Crop;
-
-            return (
-              <StyledPContainer key={cropId}>
-                <StyledPItem width="30%">
-                  <p>{cropId === '75' ? 'Blueberry' : 'Raspberry'}</p>
-                </StyledPItem>
-                <StyledPItem width="30%">
-                  <StyledPItemList>
-                    <p>{zeroConstant}</p>
-                    <p>{zeroConstant}</p>
-                    <p>{zeroConstant}</p>
-                  </StyledPItemList>
-                </StyledPItem>
-              </StyledPContainer>
-            );
-          })}
-
-          {hasFertilizerAdded && (
-            <>
-              <StyledH4HeaderItem width="30%">
-                <h4>Fertilizer</h4>
-              </StyledH4HeaderItem>
-              {farmDetails.Fields[selectedFieldIndex]?.Nutrients.map(
-                (fertilizer: FertilizerInterface) => {
-                  const { fertilizerId } = fertilizer;
-                  return (
-                    <StyledPContainer key={`Fertilizer-${fertilizer.id}-${fertilizerId}`}>
-                      <StyledPItem width="30%">
-                        <p>{fertilizerId}</p>
-                      </StyledPItem>
-                      <StyledPItem width="30%">
-                        <StyledPItemList>
-                          <p>{zeroConstant}</p>
-                          <p>{zeroConstant}</p>
-                          <p>{zeroConstant}</p>
-                        </StyledPItemList>
-                      </StyledPItem>
-                    </StyledPContainer>
-                  );
-                },
-              )}
-            </>
-          )}
-          <StyledDivider />
-          <StyledPContainer>
-            <StyledPItem width="30%">
-              <p>Balance</p>
-            </StyledPItem>
-            <StyledPItem width="30%">
-              <StyledPItemList>
-                <p>{zeroConstant}</p>
-                <p>{zeroConstant}</p>
-                <p>{zeroConstant}</p>
-              </StyledPItemList>
-            </StyledPItem>
-          </StyledPContainer>
-        </>
-      </MobileView>
     </>
   );
 };
