a1ebbb335e076862094f7c3b7b5ebdf321f1f6d7
kcaparas1630 <kcaparas1630@gmail.com>
2024-07-30 20:43:54 -0700

    format

--- frontend/src/Commons/InformationIcons/InformationIcons.tsx
+++ frontend/src/Commons/InformationIcons/InformationIcons.tsx
@@ -1,4 +1,4 @@
-import { useState, Dispatch, SetStateAction } from 'react';
+import { Dispatch, SetStateAction } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircleInfo } from '@fortawesome/free-solid-svg-icons';
 import {
