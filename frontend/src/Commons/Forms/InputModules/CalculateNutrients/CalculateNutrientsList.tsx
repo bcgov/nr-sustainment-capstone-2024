5cdc25e72deb5a6b60fb3d8ec71c2b7bce95a48e
GDamaso <80939533+GDamaso@users.noreply.github.com>
2024-08-07 17:08:10 -0700

    chore: Bb 290 restructure calculate nutrients for mobile first (#340)
    * Changed divs into a grid for CalculateNutrientsList
    
    * Update StyledDivider to use Mockups color
    
    * Change from grid to table
    
    * Fix StyledDivider
    
    * Cleanup
    
    * Use design token for color on table stripes
    
    * Fixed weird stripes for two crops

--- frontend/src/Commons/Forms/InputModules/CalculateNutrients/CalculateNutrientsList.tsx
+++ frontend/src/Commons/Forms/InputModules/CalculateNutrients/CalculateNutrientsList.tsx
@@ -7,15 +7,12 @@ import MainBalanceInterface from '@Interface/MainBalanceInterface';
 import FieldDetailInterface from '@Interface/FieldDetailsInterface';
 import getFertilizerOption from '@Utils/getFertID';
 import {
-  StyledH3HeaderContainer,
-  StyledH3HeaderItem,
-  StyledH4HeaderContainer,
-  StyledH4HeaderItem,
-  StyledH4HeaderList,
-  StyledPItem,
-  StyledPItemList,
-  StyledPContainer,
-  DesktopView,
+  CalcList,
+  StyledRemoval,
+  StyledAgronomic,
+  StyledTable,
+  StyledH3Item,
+  StyledH3Container,
 } from './CalculateNutrientsList.styles';
 import { StyledDivider } from '../ListComponent.styles';
 
@@ -33,156 +30,276 @@ const CalculationList: FC<CalculationListProps> = ({ field, cropBalances, result
 
   const getResultIconColor = (nutrient: number) => (nutrient >= 0 ? '#42814A' : '#F8BB47');
   return (
-    <DesktopView>
-      <StyledH3HeaderContainer>
-        <StyledH3HeaderItem width="35%">
-          <h3>&nbsp;</h3>
-        </StyledH3HeaderItem>
-        <StyledH3HeaderItem width="30%">
-          <h3>Argonomic (lb/ac)</h3>
-        </StyledH3HeaderItem>
-        <StyledH3HeaderItem width="30%">
-          <h3>Crop Removal (lb/ac)</h3>
-        </StyledH3HeaderItem>
-      </StyledH3HeaderContainer>
-      <StyledH4HeaderContainer>
-        <StyledH4HeaderItem width="30%">
-          <h4>Crop</h4>
-        </StyledH4HeaderItem>
+    <CalcList>
+      <StyledAgronomic>
+        <StyledH3Container>
+          <StyledH3Item className="blankSpace">
+            <h3>&nbsp;</h3>
+          </StyledH3Item>
+          <StyledH3Item>
+            <h3>Agronomic (lb/ac)</h3>
+          </StyledH3Item>
+        </StyledH3Container>
 
-        <StyledH4HeaderItem width="30%">
-          <StyledH4HeaderList>
-            <h4>N</h4>
-            <h4>P2O5</h4>
-            <h4>K2O</h4>
-          </StyledH4HeaderList>
-        </StyledH4HeaderItem>
+        <StyledTable twoCrops={field.Crops.length > 1}>
+          <thead>
+            <tr>
+              <th>
+                <h4 className="col1">Crop</h4>
+              </th>
+              <th>
+                <h4>N</h4>
+              </th>
+              <th>
+                <h4>P2O5</h4>
+              </th>
+              <th>
+                <h4>K2O</h4>
+              </th>
+            </tr>
+          </thead>
 
-        <StyledH4HeaderItem width="30%">
-          <StyledH4HeaderList>
-            <h4>N</h4>
-            <h4>P2O5</h4>
-            <h4>K2O</h4>
-          </StyledH4HeaderList>
-        </StyledH4HeaderItem>
-      </StyledH4HeaderContainer>
-
-      {field.Crops.map((crop: CropsDetailsInterface, index: number) => (
-        // eslint-disable-next-line react/no-array-index-key
-        <StyledPContainer key={`${crop}-${index}`}>
-          <StyledPItem width="30%">
-            <p>{crop.cropId === '75' ? 'Blueberry' : 'Raspberry'}</p>
-          </StyledPItem>
-          <StyledPItem width="30%">
-            <StyledPItemList>
-              <p>{cropBalances[index]?.agronomic?.N ?? zeroConstant}</p>
-              <p>{cropBalances[index]?.agronomic?.P ?? zeroConstant}</p>
-              <p>{cropBalances[index]?.agronomic?.K ?? zeroConstant}</p>
-            </StyledPItemList>
-          </StyledPItem>
-          <StyledPItem width="30%">
-            <StyledPItemList>
-              <p>{cropBalances[index]?.cropRemoval?.N ?? zeroConstant}</p>
-              <p>{cropBalances[index]?.cropRemoval?.P ?? zeroConstant}</p>
-              <p>{cropBalances[index]?.cropRemoval?.K ?? zeroConstant}</p>
-            </StyledPItemList>
-          </StyledPItem>
-        </StyledPContainer>
-      ))}
-
-      {field.Nutrients?.nutrientFertilizers?.length > 0 && (
-        <>
-          <StyledH4HeaderItem width="30%">
-            <h4>Fertilizer</h4>
-          </StyledH4HeaderItem>
-          {field.Nutrients?.nutrientFertilizers.map(
-            (fertilizer: FertilizerInterface, idx: number) => (
-              // Couldn't fix this ESLint rule since we allow for more then one of the same fert to be added
-              // Order is probably never changing without a rerender, which is the problem this rule tries to avoid
+          <tbody>
+            {field.Crops.map((crop: CropsDetailsInterface, index: number) => (
               // eslint-disable-next-line react/no-array-index-key
-              <StyledPContainer key={`${fertilizer.fertilizerId}-${idx}`}>
-                <StyledPItem width="30%">
-                  <p>
-                    {getFertilizerOption(fertilizer.fertilizerId.toString())?.label ??
-                      fertilizer.fertilizerId}
+              <tr key={`${crop}-${index}`}>
+                <td>
+                  <p className="col1">{crop.cropId === '75' ? 'Blueberry' : 'Raspberry'}</p>
+                </td>
+                <td>
+                  <p>{cropBalances[index]?.agronomic?.N ?? zeroConstant}</p>
+                </td>
+                <td>
+                  <p>{cropBalances[index]?.agronomic?.P ?? zeroConstant}</p>
+                </td>
+                <td>
+                  <p>{cropBalances[index]?.agronomic?.K ?? zeroConstant}</p>
+                </td>
+              </tr>
+            ))}
+
+            {field.Nutrients?.nutrientFertilizers?.length > 0 && (
+              <>
+                <tr>
+                  <td>
+                    <h4 className="col1">Fertilizer</h4>
+                  </td>
+                  <td>
+                    <h4 className="blankSpace">&nbsp;</h4>
+                  </td>
+                </tr>
+
+                {field.Nutrients?.nutrientFertilizers.map(
+                  (fertilizer: FertilizerInterface, idx: number) => (
+                    // eslint-disable-next-line react/no-array-index-key
+                    <tr key={`${fertilizer.fertilizerId}-${idx}`}>
+                      <td>
+                        <p className="col1">
+                          {getFertilizerOption(fertilizer.fertilizerId.toString())?.label ??
+                            fertilizer.fertilizerId}
+                        </p>
+                      </td>
+                      <td>
+                        <p>{fertilizer.fertN}</p>
+                      </td>
+                      <td>
+                        <p>{fertilizer.fertP2o5}</p>
+                      </td>
+                      <td>
+                        <p>{fertilizer.fertK2o}</p>
+                      </td>
+                    </tr>
+                  ),
+                )}
+              </>
+            )}
+
+            <tr>
+              <td>
+                <StyledDivider />
+              </td>
+              <td>
+                <StyledDivider />
+              </td>
+              <td>
+                <StyledDivider />
+              </td>
+              <td>
+                <StyledDivider />
+              </td>
+            </tr>
+          </tbody>
+
+          <tfoot>
+            <tr>
+              <td>
+                <p className="col1">Balance</p>
+              </td>
+              <td>
+                <p>
+                  <FontAwesomeIcon
+                    icon={getResultsIcon(resultBalance.agronomic.N)}
+                    color={getResultIconColor(resultBalance.agronomic.N)}
+                  />
+                  {resultBalance.agronomic.N}
+                </p>
+              </td>
+              <td>
+                <p>
+                  <FontAwesomeIcon
+                    icon={getResultsIcon(resultBalance.agronomic.P)}
+                    color={getResultIconColor(resultBalance.agronomic.P)}
+                  />
+                  {resultBalance.agronomic.P}
+                </p>
+              </td>
+              <td>
+                <p>
+                  <FontAwesomeIcon
+                    icon={getResultsIcon(resultBalance.agronomic.K)}
+                    color={getResultIconColor(resultBalance.agronomic.K)}
+                  />
+                  {resultBalance.agronomic.K}
+                </p>
+              </td>
+            </tr>
+          </tfoot>
+        </StyledTable>
+      </StyledAgronomic>
+
+      <StyledRemoval>
+        <StyledH3Item>
+          <h3>Crop Removal (lb/ac)</h3>
+        </StyledH3Item>
+
+        <StyledTable twoCrops={field.Crops.length > 1}>
+          <thead>
+            <tr>
+              <th>
+                <h4 className="cropRemovalCol1 col1">Crop</h4>
+              </th>
+              <th>
+                <h4>N</h4>
+              </th>
+              <th>
+                <h4>P2O5</h4>
+              </th>
+              <th>
+                <h4>K2O</h4>
+              </th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {field.Crops.map((crop: CropsDetailsInterface, index: number) => (
+              // eslint-disable-next-line react/no-array-index-key
+              <tr key={`removal-${crop}-${index}`}>
+                <td>
+                  <p className="cropRemovalCol1 col1">
+                    {crop.cropId === '75' ? 'Blueberry' : 'Raspberry'}
                   </p>
-                </StyledPItem>
-                <StyledPItem width="30%">
-                  <StyledPItemList>
-                    <p>{fertilizer.fertN}</p>
-                    <p>{fertilizer.fertP2o5}</p>
-                    <p>{fertilizer.fertK2o}</p>
-                  </StyledPItemList>
-                </StyledPItem>
-                <StyledPItem width="30%">
-                  <StyledPItemList>
-                    <p>{fertilizer.fertN}</p>
-                    <p>{fertilizer.fertP2o5}</p>
-                    <p>{fertilizer.fertK2o}</p>
-                  </StyledPItemList>
-                </StyledPItem>
-              </StyledPContainer>
-            ),
-          )}
-        </>
-      )}
-      <StyledDivider />
-      <StyledPContainer>
-        <StyledPItem width="30%">
-          <p>Balance</p>
-        </StyledPItem>
-        <StyledPItem width="30%">
-          <StyledPItemList>
-            <p>
-              <FontAwesomeIcon
-                icon={getResultsIcon(resultBalance.agronomic.N)}
-                color={getResultIconColor(resultBalance.agronomic.N)}
-              />
-              {resultBalance.agronomic.N}
-            </p>
-            <p>
-              <FontAwesomeIcon
-                icon={getResultsIcon(resultBalance.agronomic.P)}
-                color={getResultIconColor(resultBalance.agronomic.P)}
-              />
-              {resultBalance.agronomic.P}
-            </p>
-            <p>
-              <FontAwesomeIcon
-                icon={getResultsIcon(resultBalance.agronomic.K)}
-                color={getResultIconColor(resultBalance.agronomic.K)}
-              />
-              {resultBalance.agronomic.K}
-            </p>
-          </StyledPItemList>
-        </StyledPItem>
-        <StyledPItem width="30%">
-          <StyledPItemList>
-            <p>
-              <FontAwesomeIcon
-                icon={getResultsIcon(resultBalance.cropRemoval.N || 0)}
-                color={getResultIconColor(resultBalance.cropRemoval.N || 0)}
-              />
-              {resultBalance.cropRemoval.N || 0}
-            </p>
-            <p>
-              <FontAwesomeIcon
-                icon={getResultsIcon(resultBalance.cropRemoval.P)}
-                color={getResultIconColor(resultBalance.cropRemoval.P)}
-              />
-              {resultBalance.cropRemoval.P}
-            </p>
-            <p>
-              <FontAwesomeIcon
-                icon={getResultsIcon(resultBalance.cropRemoval.K)}
-                color={getResultIconColor(resultBalance.cropRemoval.K)}
-              />
-              {resultBalance.cropRemoval.K}
-            </p>
-          </StyledPItemList>
-        </StyledPItem>
-      </StyledPContainer>
-    </DesktopView>
+                </td>
+                <td>
+                  <p>{cropBalances[index]?.cropRemoval?.N ?? zeroConstant}</p>
+                </td>
+                <td>
+                  <p>{cropBalances[index]?.cropRemoval?.P ?? zeroConstant}</p>
+                </td>
+                <td>
+                  <p>{cropBalances[index]?.cropRemoval?.K ?? zeroConstant}</p>
+                </td>
+              </tr>
+            ))}
+
+            {field.Nutrients?.nutrientFertilizers?.length > 0 && (
+              <>
+                <tr>
+                  <td>
+                    <h4 className=" cropRemovalCol1 col1">Fertilizer</h4>
+                  </td>
+                  <td>
+                    <h4>&nbsp;</h4>
+                  </td>
+                </tr>
+
+                {field.Nutrients?.nutrientFertilizers.map(
+                  (fertilizer: FertilizerInterface, idx: number) => (
+                    // eslint-disable-next-line react/no-array-index-key
+                    <tr key={`${fertilizer.fertilizerId}-${idx}`}>
+                      <td>
+                        <p className="cropRemovalCol1 col1">
+                          {getFertilizerOption(fertilizer.fertilizerId.toString())?.label ??
+                            fertilizer.fertilizerId}
+                        </p>
+                      </td>
+                      <td>
+                        <p>{fertilizer.fertN}</p>
+                      </td>
+                      <td>
+                        <p>{fertilizer.fertP2o5}</p>
+                      </td>
+                      <td>
+                        <p>{fertilizer.fertK2o}</p>
+                      </td>
+                    </tr>
+                  ),
+                )}
+              </>
+            )}
+
+            <tr>
+              <td>
+                <StyledDivider />
+              </td>
+              <td>
+                <StyledDivider />
+              </td>
+              <td>
+                <StyledDivider />
+              </td>
+              <td>
+                <StyledDivider />
+              </td>
+            </tr>
+          </tbody>
+
+          <tfoot>
+            <tr>
+              <td>
+                <p className="cropRemovalCol1 col1">Balance</p>
+              </td>
+              <td>
+                <p>
+                  <FontAwesomeIcon
+                    icon={getResultsIcon(resultBalance.cropRemoval.N || 0)}
+                    color={getResultIconColor(resultBalance.cropRemoval.N || 0)}
+                  />
+                  {resultBalance.cropRemoval.N || 0}
+                </p>
+              </td>
+              <td>
+                <p>
+                  <FontAwesomeIcon
+                    icon={getResultsIcon(resultBalance.cropRemoval.P)}
+                    color={getResultIconColor(resultBalance.cropRemoval.P)}
+                  />
+                  {resultBalance.cropRemoval.P}
+                </p>
+              </td>
+              <td>
+                <p>
+                  <FontAwesomeIcon
+                    icon={getResultsIcon(resultBalance.cropRemoval.K)}
+                    color={getResultIconColor(resultBalance.cropRemoval.K)}
+                  />
+                  {resultBalance.cropRemoval.K}
+                </p>
+              </td>
+            </tr>
+          </tfoot>
+        </StyledTable>
+      </StyledRemoval>
+    </CalcList>
   );
 };
 
